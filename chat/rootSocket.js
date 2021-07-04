fbd76f34087b79fbd790aac80a9a2620825ecad0
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-07-04 23:12:13 +0700

    add comment

--- chat/rootSocket.js
+++ chat/rootSocket.js
@@ -1,18 +1,23 @@
 module.exports = {
     rootSocket:function(io)  {
+      //lắng nghe sự kiện kết nối lên server
         io.on('connection', function(socket){
             console.log("Connected user ",socket.id);
+            //lắng nghe các sự kiện client gửi lên server với data là message
             socket.on('connect user', function(user){
               io.emit('connect user', user);
             });
-          
+          //lắng nghe các sự kiện client gửi lên server với data là message
             socket.on('on typing', function(typing){
               console.log("Typing.... ");
+              //gửi message đến client
               io.emit('on typing', typing);
             });
-          
+           //lắng nghe các sự kiện client gửi lên server với data là message
+
             socket.on('chat message', function(msg){
               console.log("Message " + msg);
+               //gửi message đến client
               io.emit('chat message', msg);
             });
             /*socket.on('chat message', function(msg){
